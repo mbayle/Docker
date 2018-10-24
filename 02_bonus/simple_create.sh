ff00c077d21abfcca7f311d99d9df34f7e381892
mbayle <cerbertte12@gmail.com>
2018-10-24 06:35:00 +0200

    Delete simple_create.sh

--- 02_bonus/simple_create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Please enter the name of the new machine : "
-name=""
-while [ -z "$name" ]
-do
-		read -r name
-	done
-docker-machine create -d virtualbox $name
-eval "$(docker-machine env $name)"
